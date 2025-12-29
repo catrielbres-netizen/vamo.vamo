00c86b7720fbfe09721d86453c5cb40c6b834183
Catriel César Leandro Bres <catrielbres@gmail.com>
2025-12-29 15:11:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6697160840-7c67f",
+  "appId": "1:68554242118:web:93c2b08fdb55d657167247",
+  "apiKey": "AIzaSyDOkw1zuu8JZu2zGwn_YUWK1az4zphC9PA",
+  "authDomain": "studio-6697160840-7c67f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "68554242118"
+};
