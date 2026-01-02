35d503e61bb176f99a88cdce5bb8555f2afbaa07
Catriel César Leandro Bres <catrielbres@gmail.com>
2026-01-02 05:01:00 +0000

    Perfecto, **este error ya es CLARÍSIMO y es el último bloqueo real**. Bu

--- next.config.js
+++ next.config.js
@@ -1,29 +1,25 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'standalone',
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
         pathname: '/**',
       },
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
