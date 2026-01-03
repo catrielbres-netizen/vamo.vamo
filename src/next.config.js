bfb3900d52a02cafb38ee3109431156608259e2c
Catriel César Leandro Bres <catrielbres@gmail.com>
2026-01-03 20:35:04 +0000

    mi conductor en prodeuccion no le llega el viaje disponible

---
+++ src/next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: [
+    '@vis.gl/react-google-maps',
+    'use-places-autocomplete'
+  ],
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
